8c4f3548646e026f0e7ada2e03a1795b76c86c4b
Raga <ragawilliam570@gmail.com>
2022-08-08 13:59:19 +0300

    Added a book.js that contains class Book

---
+++ modules/book.js
@@ -0,0 +1,20 @@
+export default class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+
+  deleteBook = (e, myLibrary) => {
+    e.target.parentElement.remove();
+    myLibrary.splice(e.target.parentElement.id, 1);
+    localStorage.setItem('book', JSON.stringify(myLibrary));
+  };
+
+  addBook = (book, myLibrary) => {
+    myLibrary.push(book);
+    localStorage.setItem('book', JSON.stringify(myLibrary));
+  }
+}
+
+// Instance of book class
+// const bk = new Book();
